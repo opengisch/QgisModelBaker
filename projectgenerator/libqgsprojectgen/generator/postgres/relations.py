6e9afc736de85febbc11139e8bb34162497776d1
gacarrillor <geotux_tuxman@linuxmail.org>
2017-05-02 17:26:35 -0500

    Filter relations by schema

--- projectgenerator/libqgsprojectgen/generator/postgres/relations.py
+++ projectgenerator/libqgsprojectgen/generator/postgres/relations.py
@@ -6,7 +6,7 @@ class PostgresRelation(Relation):
     pass
 
     @classmethod
-    def find_relations(cls, layers, conn):
+    def find_relations(cls, layers, conn, schema):
 
         mapped_layers = {layer.table_name : layer for layer in layers}
 
@@ -15,13 +15,13 @@ class PostgresRelation(Relation):
         cur.execute("""SELECT RC.CONSTRAINT_NAME, KCU1.TABLE_NAME AS referencing_table_name, KCU1.COLUMN_NAME AS referencing_column_name, KCU2.CONSTRAINT_SCHEMA, KCU2.TABLE_NAME AS referenced_table_name, KCU2.COLUMN_NAME AS referenced_column_name, KCU1.ORDINAL_POSITION
                         FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS AS RC
                         INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KCU1
-                        ON KCU1.CONSTRAINT_CATALOG = RC.CONSTRAINT_CATALOG AND KCU1.CONSTRAINT_SCHEMA = RC.CONSTRAINT_SCHEMA AND KCU1.CONSTRAINT_NAME = RC.CONSTRAINT_NAME
+                        ON KCU1.CONSTRAINT_CATALOG = RC.CONSTRAINT_CATALOG AND KCU1.CONSTRAINT_SCHEMA = RC.CONSTRAINT_SCHEMA AND KCU1.CONSTRAINT_NAME = RC.CONSTRAINT_NAME AND KCU1.CONSTRAINT_SCHEMA = '{schema}'
                         INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KCU2
                           ON KCU2.CONSTRAINT_CATALOG = RC.UNIQUE_CONSTRAINT_CATALOG AND KCU2.CONSTRAINT_SCHEMA = RC.UNIQUE_CONSTRAINT_SCHEMA AND KCU2.CONSTRAINT_NAME = RC.UNIQUE_CONSTRAINT_NAME
-                          AND KCU2.ORDINAL_POSITION = KCU1.ORDINAL_POSITION
+                          AND KCU2.ORDINAL_POSITION = KCU1.ORDINAL_POSITION AND KCU2.CONSTRAINT_SCHEMA = '{schema}'
                         GROUP BY RC.CONSTRAINT_NAME, KCU1.TABLE_NAME, KCU1.COLUMN_NAME, KCU2.CONSTRAINT_SCHEMA, KCU2.TABLE_NAME, KCU2.COLUMN_NAME, KCU1.ORDINAL_POSITION
                         ORDER BY KCU1.ORDINAL_POSITION
-                        """)
+                        """.format(schema=schema))
 
         relations = list()
 
