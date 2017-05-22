5c83c0cff6a9503f02e914c75a474d2778e230f9
gacarrillor <geotux_tuxman@linuxmail.org>
2017-05-22 15:11:50 -0500

    Determine whether a table is a domain

--- projectgenerator/libqgsprojectgen/dataobjects/layers.py
+++ projectgenerator/libqgsprojectgen/dataobjects/layers.py
@@ -22,22 +22,25 @@ from qgis.core import QgsVectorLayer, QgsDataSourceUri, QgsWkbTypes
 
 
 class Layer:
-    def __init__(self, provider, uri):
+    def __init__(self, provider, uri, is_domain=False):
         self.provider = provider
         self.uri = uri
         self.__layer = None
         self.fields = list()
+        self.is_domain = is_domain
 
     def dump(self):
         definition = dict()
         definition['provider'] = self.provider
         definition['uri'] = self.uri
+        definition['isdomain'] = self.is_domain
 
         return definition
 
     def load(self, definition):
         self.provider = definition['provider']
         self.uri = definition['uri']
+        self.is_domain = definition['isdomain']
 
     def create(self):
         if not self.__layer:
