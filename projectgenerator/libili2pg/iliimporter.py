9a039b68fd8ebd1eaa20b33a56e5019b1eb6308f
gacarrillor <geotux_tuxman@linuxmail.org>
2017-05-22 17:03:50 -0500

    Use os.pathsep to split JAVA_HOME paths

--- projectgenerator/libili2pg/iliimporter.py
+++ projectgenerator/libili2pg/iliimporter.py
@@ -121,7 +121,7 @@ class Importer(QObject):
             # By default try JAVA_HOME and PATH
             java_paths = []
             if 'JAVA_HOME' in os.environ:
-                paths = os.environ['JAVA_HOME'].split(";")
+                paths = os.environ['JAVA_HOME'].split(os.pathsep)
                 for path in paths:
                     java_paths += [os.path.join(path.replace("\"","").replace("'",""), 'java')]
             java_paths += ['java']
