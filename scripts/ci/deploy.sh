de8c338a67b5c180d5c3e357ff7a2213902d8b9e
Matthias Kuhn <matthias@opengis.ch>
2017-07-19 18:04:29 +0200

    Fix deploy

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -3,8 +3,7 @@
 set -e
 
 PLUGIN_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d'/' -f 2)
-#METADATA_VERSION=v$(grep -Po "(?<=^version=).*" $PLUGIN_NAME/metadata.txt)
-METADATA_VERSION=v1.0
+METADATA_VERSION=v$(grep -Po "(?<=^version=).*" $PLUGIN_NAME/metadata.txt)
 if [ "$METADATA_VERSION" != "${TRAVIS_TAG}" ]; then
   echo -e "\e[31mVersion tag in metadata ($METADATA_VERSION) and in git tag ($TRAVIS_TAG) do not match.\e[0m"
   echo -e "\e[31mThis will not be deployed.\e[0m"
