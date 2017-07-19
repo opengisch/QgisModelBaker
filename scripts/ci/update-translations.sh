63c81e381c5edbc558bf9889f90b696b3a84e3a8
Matthias Kuhn <matthias@opengis.ch>
2017-07-19 16:58:52 +0200

    Use API Token for Transifex

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
-  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = $TRANSIFEX_USERNAME\npassword = $TRANSIFEX_PASSWORD\ntoken =\n" > ~/.transifexrc
+  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = $TRANSIFEX_API_TOKEN\ntoken =\n" > ~/.transifexrc
   make gettext
   tx push -s
 fi
