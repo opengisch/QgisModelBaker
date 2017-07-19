cbff19f0cfb813c829cd1d448f4877834c8ac4e1
Matthias Kuhn <matthias@opengis.ch>
2017-07-19 17:24:34 +0200

    Conditional translation compile

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -2,11 +2,9 @@
 if test "$TRAVIS_SECURE_ENV_VARS" = "true"; # -a "$TRAVIS_BRANCH" = "master";
 then
   echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = $TRANSIFEX_API_TOKEN\ntoken =\n" > ~/.transifexrc
-  make gettext
-  tx push -s
-fi
 
-./scripts/update-strings.sh en
-./scripts/push-transifex-translations.sh
-./scripts/pull-transifex-translations.sh
-./scripts/compile-strings.sh i18n/*.ts
+  ./scripts/update-strings.sh en
+  ./scripts/push-transifex-translations.sh
+  ./scripts/pull-transifex-translations.sh
+  ./scripts/compile-strings.sh i18n/*.ts
+fi
