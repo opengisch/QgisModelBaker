47992cf3f397d333f4193e77fbc575464c5a61bc
Matthias Kuhn <matthias@opengis.ch>
2017-07-20 08:31:22 +0200

    Fix bash syntax

--- scripts/ci/update-translations.sh
+++ scripts/ci/update-translations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # If we are on the master branch and not on a fork, upload translation strings
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
+if [[ ${TRAVIS_SECURE_ENV_VARS} == "true" && ${TRAVIS_BRANCH} == "master" ]];
 then
-  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = $TRANSIFEX_API_TOKEN\ntoken =\n" > ~/.transifexrc
+  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = ${TRANSIFEX_API_TOKEN}\ntoken =\n" > ~/.transifexrc
 
   ./scripts/update-strings.sh en
   ./scripts/push-transifex-translations.sh
@@ -12,9 +12,9 @@ else
 fi
 
 # If we are doing a release, pull in the latest strings from transifex
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a [ ${RELEASE_VERSION+x} ] ;
+if [[ ${TRAVIS_SECURE_ENV_VARS} == "true" && ${RELEASE_VERSION+x} ]] ;
 then
-  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = $TRANSIFEX_API_TOKEN\ntoken =\n" > ~/.transifexrc
+  echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = api\npassword = ${TRANSIFEX_API_TOKEN}\ntoken =\n" > ~/.transifexrc
 
   ./scripts/pull-transifex-translations.sh
   ./scripts/compile-strings.sh i18n/*.ts
