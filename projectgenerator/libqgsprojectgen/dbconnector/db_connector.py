c657a8b2d6857641d5e84606dbac16ae48ec72c2
gacarrillor <geotux_tuxman@linuxmail.org>
2017-11-24 10:17:41 -0500

    Specify 'iliCode' field name for each DB type

--- projectgenerator/libqgsprojectgen/dbconnector/db_connector.py
+++ projectgenerator/libqgsprojectgen/dbconnector/db_connector.py
@@ -23,6 +23,7 @@ class DBConnector:
         self.QGIS_DATE_TYPE = 'date'
         self.QGIS_TIME_TYPE = 'time'
         self.QGIS_DATE_TIME_TYPE = 'datetime'
+        self.iliCodeName = '' # For Domain-Class relations, specific for each DB
 
     def map_data_types(self, data_type):
         '''Map provider date/time types to QGIS date/time types'''
--- projectgenerator/libqgsprojectgen/dbconnector/gpkg_connector.py
+++ projectgenerator/libqgsprojectgen/dbconnector/gpkg_connector.py
@@ -30,6 +30,7 @@ class GPKGConnector(DBConnector):
         self.conn.row_factory = sqlite3.Row
         self._bMetadataTable = self._metadata_exists()
         self._tables_info = self._get_tables_info()
+        self.iliCodeName = 'iliCode'
 
     def map_data_types(self, data_type):
         '''GPKG date/time types correspond to QGIS date/time types'''
