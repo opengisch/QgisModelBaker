48a37dbfdc07ad0504cb7cdc7588b26a450ac410
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-03-27 17:11:51 -0500

    Adding test_postgres_db_without_schema, test_postgres_db_with_empty_schema, test_empty_geopackage_db, test_not_empty_ogr_geopackage_db and test_not_empty_geopackage_db

--- projectgenerator/tests/test_projectgen_simple.py
+++ projectgenerator/tests/test_projectgen_simple.py
@@ -26,6 +26,7 @@ import nose2
 import psycopg2
 
 from projectgenerator.libqgsprojectgen.dataobjects import Project
+from projectgenerator.tests.utils import testdata_path
 from qgis.testing import unittest, start_app
 from qgis.core import QgsProject, QgsEditFormConfig
 from projectgenerator.libqgsprojectgen.generator.generator import Generator
@@ -45,16 +46,37 @@ class TestProjectGen(unittest.TestCase):
         try:
             generator = Generator('ili2pg', 'dbname=not_exists_database user=docker password=docker host=postgres', 'smart1', '')
             self.assertFalse(True) # must be failed
-        except Exception as e:
+        except psycopg2.OperationalError as e:
+            # psycopg2.OperationalError: FATAL:  database "not_exists_database" does not exist
             self.assertIsNone(generator)
 
-    def test_empty_database(self):
+    def test_postgres_db_without_schema(self):
+        generator = Generator('ili2pg', 'dbname=gis user=docker password=docker host=postgres', 'smart1')
+        self.assertIsNotNone(generator)
+        self.assertEqual(len(generator.layers()), 0)
+
+    def test_postgres_db_with_empty_schema(self):
         generator = Generator('ili2pg', 'dbname=gis user=docker password=docker host=postgres', 'smart1', 'empty_schema')
         cur = generator._db_connector.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute("""
         CREATE SCHEMA IF NOT EXISTS empty_schema;
         """)
 
+        self.assertEqual(len(generator.layers()), 0)
+
+    def test_postgres_db_with_not_empty_and_not_interlis_schema(self):
+        pass
+
+    def test_empty_geopackage_db(self):
+        generator = Generator('ili2gpkg', testdata_path('geopackage/test_empty.gpkg'), 'smart2')
+        self.assertEqual(len(generator.layers()), 0)
+
+    def test_not_empty_ogr_geopackage_db(self):
+        generator = Generator('ili2gpkg', testdata_path('geopackage/test_ogr_empty.gpkg'), 'smart2')
+        self.assertEqual(len(generator.layers()), 0)
+
+    def test_not_empty_geopackage_db(self):
+        generator = Generator('ili2gpkg', testdata_path('geopackage/test_relations.gpkg'), 'smart2')
         available_layers = generator.layers()
         relations = generator.relations(available_layers)
         legend = generator.legend(available_layers)
@@ -65,8 +87,11 @@ class TestProjectGen(unittest.TestCase):
         project.legend = legend
         project.post_generate()
 
+        # https://qgis.org/api/classQgsProject.html
         qgis_project = QgsProject.instance()
         project.create(None, qgis_project)
+        self.assertEqual(len(project.layers), 10)
+        self.assertEqual(len(project.relations), 1)
 
     @classmethod
     def tearDownClass(cls):
