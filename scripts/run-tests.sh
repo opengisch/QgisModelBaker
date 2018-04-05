e48f17bc357dfad5551c2c1a16f250b8737242bd
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-04-05 09:00:10 -0500

    shortest and do the same

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ROOTPATH=$(git rev-parse --show-toplevel)
-
-pushd $ROOTPATH
+cd $(dirname $0)/..
 export TRAVIS_BUILD_DIR=$PWD
 docker-compose -f .docker/docker-compose.travis.yml run qgis /usr/src/.docker/run-docker-tests.sh
 docker-compose -f .docker/docker-compose.travis.yml rm -s -f
-popd
