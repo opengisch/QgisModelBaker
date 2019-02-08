1a7d50a1e511a2a77a904bb9bbc40d31e2333ba3
yesidpol <yesidpol.3@gmail.com>
2019-02-08 11:46:40 -0500

    SQL Server support (see #324)

---
+++ QgisModelBaker/gui/panel/mssql_config_panel.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+    begin                :    10/05/19
+    git sha              :    :%H$
+    copyright            :    (C) 2019 by Yesid Polania (BSF Swissphoto)
+    email                :    yesidpol.3@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from qgis.PyQt.QtCore import pyqtSignal
+
+from .db_config_panel import DbConfigPanel
+from ...utils import get_ui_class
+from QgisModelBaker.utils.qt_utils import (
+    Validators,
+    NonEmptyStringValidator)
+from QgisModelBaker.utils.mssql_utils import get_odbc_drivers
+from QgisModelBaker.libili2db.globals import DbActionType
+
+WIDGET_UI = get_ui_class('mssql_settings_panel.ui')
+
+class MssqlConfigPanel(DbConfigPanel, WIDGET_UI):
+
+    notify_fields_modified = pyqtSignal(str)
+
+    def __init__(self, parent, db_action_type):
+        DbConfigPanel.__init__(self, parent, db_action_type)
+        self.setupUi(self)
+
+        for item_odbc_driver in get_odbc_drivers():
+            self.mssql_odbc_driver.addItem(item_odbc_driver)
+
+        # define validators
+        self.validators = Validators()
+        nonEmptyValidator = NonEmptyStringValidator()
+
+        self.mssql_host_line_edit.setValidator(nonEmptyValidator)
+        self.mssql_database_line_edit.setValidator(nonEmptyValidator)
+        self.mssql_user_line_edit.setValidator(nonEmptyValidator)
+
+        self.mssql_host_line_edit.textChanged.connect(
+            self.validators.validate_line_edits)
+        self.mssql_host_line_edit.textChanged.emit(self.mssql_host_line_edit.text())
+        self.mssql_database_line_edit.textChanged.connect(
+            self.validators.validate_line_edits)
+        self.mssql_database_line_edit.textChanged.emit(
+            self.mssql_database_line_edit.text())
+        self.mssql_user_line_edit.textChanged.connect(
+            self.validators.validate_line_edits)
+        self.mssql_user_line_edit.textChanged.emit(self.mssql_user_line_edit.text())
+
+        self.mssql_host_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_instance_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_port_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_database_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_schema_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_user_line_edit.textChanged.connect(self.notify_fields_modified)
+        self.mssql_password_line_edit.textChanged.connect(self.notify_fields_modified)
+
+    def _show_panel(self):
+        if self.interlis_mode:
+            self.mssql_schema_line_edit.setPlaceholderText(
+                self.tr("[Leave empty to create a default schema]"))
+        else:
+            if self._db_action_type == DbActionType.IMPORT_DATA:
+                self.mssql_schema_line_edit.setPlaceholderText(
+                    self.tr("[Leave empty to import data into a default schema]"))
+            else:
+                self.mssql_schema_line_edit.setPlaceholderText(
+                    self.tr("[Leave empty to load all schemas in the database]"))
+
+    def get_fields(self, configuration):
+        configuration.dbhost = self.mssql_host_line_edit.text().strip()
+        configuration.dbinstance = self.mssql_instance_line_edit.text().strip()
+        configuration.dbport = self.mssql_port_line_edit.text().strip()
+        configuration.dbusr = self.mssql_user_line_edit.text().strip()
+        configuration.database = self.mssql_database_line_edit.text().strip()
+        configuration.dbschema = self.mssql_schema_line_edit.text().strip().lower()
+        configuration.dbpwd = self.mssql_password_line_edit.text()
+        configuration.db_odbc_driver = self.mssql_odbc_driver.currentText()
+
+    def set_fields(self, configuration):
+        self.mssql_host_line_edit.setText(configuration.dbhost)
+        self.mssql_instance_line_edit.setText(configuration.dbinstance)
+        self.mssql_port_line_edit.setText(configuration.dbport)
+        self.mssql_user_line_edit.setText(configuration.dbusr)
+        self.mssql_database_line_edit.setText(configuration.database)
+        self.mssql_schema_line_edit.setText(configuration.dbschema)
+        self.mssql_password_line_edit.setText(configuration.dbpwd)
+
+        index = self.mssql_odbc_driver.findText(configuration.db_odbc_driver)
+        if index != -1:
+            self.mssql_odbc_driver.setCurrentIndex(index)
+
+    def is_valid(self):
+        result = False
+        message = ''
+        if not self.mssql_host_line_edit.text().strip():
+            message = self.tr("Please set a host before creating the project.")
+            self.mssql_host_line_edit.setFocus()
+        elif not "{}".format(self.mssql_database_line_edit.text().strip()):
+            message = self.tr("Please set a database before creating the project.")
+            self.mssql_database_line_edit.setFocus()
+        elif not self.mssql_user_line_edit.text().strip():
+            message = self.tr("Please set a database user before creating the project.")
+            self.mssql_user_line_edit.setFocus()
+        elif not self.mssql_odbc_driver.currentText():
+            message = self.tr("Please set a odbc driver before creating the project.")
+        else:
+            result = True
+
+        return result, message
--- QgisModelBaker/tests/test_domain_class_relations.py
+++ QgisModelBaker/tests/test_domain_class_relations.py
@@ -195,6 +195,86 @@ class TestDomainClassRelation(unittest.TestCase):
         for expected_relation in expected_relations:
             self.assertIn(expected_relation, relations_dicts)
 
+    def test_domain_class_relations_mssql(self):
+        # Schema Import
+        importer = iliimporter.Importer()
+        importer.tool = DbIliMode.ili2mssql
+        importer.configuration = iliimporter_config(
+            importer.tool, 'ilimodels/CIAF_LADM')
+        importer.configuration.ilimodels = 'CIAF_LADM'
+        importer.configuration.dbschema = 'ciaf_ladm_{:%Y%m%d%H%M%S%f}'.format(
+            datetime.datetime.now())
+        importer.configuration.epsg = 3116
+        importer.configuration.inheritance = 'smart2'
+        importer.stdout.connect(self.print_info)
+        importer.stderr.connect(self.print_error)
+        self.assertEqual(importer.run(), iliimporter.Importer.SUCCESS)
+
+        uri = 'DRIVER={drv};SERVER={server};DATABASE={db};UID={uid};PWD={pwd}'\
+            .format(drv="{ODBC Driver 17 for SQL Server}",
+                    server="mssql",
+                    db=importer.configuration.database,
+                    uid=importer.configuration.dbusr,
+                    pwd=importer.configuration.dbpwd)
+
+        generator = Generator(DbIliMode.ili2mssql, uri,
+                              importer.configuration.inheritance,
+                              importer.configuration.dbschema)
+
+        available_layers = generator.layers()
+        relations, _ = generator.relations(available_layers)
+
+
+        # Check domain class relations in the relations list
+        relations_dicts = list()
+        for relation in relations:
+            relations_dicts.append({"referencing_layer": relation.referencing_layer.name,
+                                    "referenced_layer": relation.referenced_layer.name,
+                                    "referencing_field": relation.referencing_field,
+                                    "referenced_field": relation.referenced_field,
+                                    "name": relation.name})
+
+        expected_relations = list()  # 6 domain-class relations are expected
+        # Domain from the same model, out of the topic
+        expected_relations.append({"referencing_layer": "avaluo",
+                                   "referenced_layer": "avaluo_usotipo",
+                                   "referencing_field": "uso",
+                                   "referenced_field": "iliCode",
+                                   "name": "avaluo_uso_avaluo_usotipo_iliCode"})
+        # Domain inherited from superclass and from another model
+        expected_relations.append({"referencing_layer": "derecho",
+                                   "referenced_layer": "col_derechotipo",
+                                   "referencing_field": "tipo",
+                                   "referenced_field": "iliCode",
+                                   "name": "derecho_tipo_col_derechotipo_iliCode"})
+        # Domain from another model
+        expected_relations.append({"referencing_layer": "persona",
+                                   "referenced_layer": "col_interesadodocumentotipo",
+                                   "referencing_field": "documento_tipo",
+                                   "referenced_field": "iliCode",
+                                   "name": "persona_documento_tipo_col_interesadodocumentotipo_iliCode"})
+        # Domain from another model
+        expected_relations.append({"referencing_layer": "persona",
+                                   "referenced_layer": "col_genero",
+                                   "referencing_field": "genero",
+                                   "referenced_field": "iliCode",
+                                   "name": "persona_genero_col_genero_iliCode"})
+        # Domain inherited from abstract class
+        expected_relations.append({"referencing_layer": "persona",
+                                   "referenced_layer": "la_interesadotipo",
+                                   "referencing_field": "tipo",
+                                   "referenced_field": "iliCode",
+                                   "name": "persona_tipo_la_interesadotipo_iliCode"})
+        # Domain inherited from abstract class
+        expected_relations.append({"referencing_layer": "predio",
+                                   "referenced_layer": "la_baunittipo",
+                                   "referencing_field": "tipo",
+                                   "referenced_field": "iliCode",
+                                   "name": "predio_tipo_la_baunittipo_iliCode"})
+
+        for expected_relation in expected_relations:
+            self.assertIn(expected_relation, relations_dicts)
+
     def test_domain_class_relations_ZG_Abfallsammelstellen_ZEBA_V1_postgis(self):
         # Schema Import
         importer = iliimporter.Importer()
