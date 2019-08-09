6d46998138e994392a9f31331ed067e5a06650e1
Germán <geotux_tuxman@linuxmail.org>
2019-08-08 22:41:40 -0500

    Update ili2gpkg version to 3.12.3
    This ili2gpkg version will be used in a FOSS4G workshop along with Model Baker

--- QgisModelBaker/libqgsprojectgen/db_factory/gpkg_factory.py
+++ QgisModelBaker/libqgsprojectgen/db_factory/gpkg_factory.py
@@ -49,7 +49,7 @@ class GpkgFactory(DbFactory):
 
         :return: str ili2gpkg version.
         """
-        return '3.11.3'
+        return '3.12.3'
 
     def get_tool_url(self):
         """Returns download url of ili2gpkg.
