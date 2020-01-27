8738eceffb3b30074dbf41beacd49c5154b72139
David Signer <david@opengis.ch>
2020-01-27 14:59:32 +0100

    fix ili2gpkg 4.4.0 link

--- QgisModelBaker/libqgsprojectgen/db_factory/gpkg_factory.py
+++ QgisModelBaker/libqgsprojectgen/db_factory/gpkg_factory.py
@@ -62,7 +62,7 @@ class GpkgFactory(DbFactory):
         """
         # remove this if when 4.4.0 is released
         if self.get_tool_version(db_ili_version) == '4.4.0':
-            return 'http://jars.interlis.ch/ch/interlis/ili2gpkg/4.4.0-SNAPSHOT/ili2gpkg-4.4.0-20200127.103509-7-bindist.zip'
+            return 'http://jars.interlis.ch/ch/interlis/ili2gpkg/4.4.0-SNAPSHOT/ili2gpkg-4.4.0-20200127.103444-7-bindist.zip'
         else:
             return 'http://www.eisenhutinformatik.ch/interlis/ili2gpkg/ili2gpkg-{version}.zip'.format(version=self.get_tool_version(db_ili_version))
 
