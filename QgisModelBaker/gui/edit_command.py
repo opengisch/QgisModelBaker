12ae179543d300085a27746fca2108dff08064ee
signedav <david@opengis.ch>
2020-07-27 17:15:14 +0200

    Edit Command Dialog opens on toolbutton in the generate project dialog After editing the ili2db command the command can be run. It creates the db accordingly. The Project is generated accordingly but takes the db parameters from the GUI (though they are changed in the command).

---
+++ QgisModelBaker/gui/edit_command.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                              -------------------
+        begin                : 27.08.2020
+        git sha              : :%H$
+        copyright            : (C) 2020 by Dave Signer
+        email                : david at opengis ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from QgisModelBaker.utils import get_ui_class
+
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QDialog, QListWidgetItem, QDialogButtonBox
+from qgis.gui import QgsGui
+
+DIALOG_UI = get_ui_class('edit_command.ui')
+
+
+class EditCommandDialog(QDialog, DIALOG_UI):
+
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        QgsGui.instance().enableAutoGeometryRestore(self)
+        self.parent = parent
+        self.command_edit.setEnabled(False)
+        self.chk_activate.stateChanged.connect(self.enable_command_edit)
+        self.button_box.button(QDialogButtonBox.Ok).setText(self.tr('Generate'))
+
+    def enable_command_edit(self, state):
+        self.command_edit.setEnabled(state)
