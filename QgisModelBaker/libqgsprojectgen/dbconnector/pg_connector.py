6eaa9ec0eae0e9336463f34775286718cd46aca3
signedav <david@opengis.ch>
2020-07-27 10:39:37 +0200

    use left join on getting relation strength information not to loose relations not having the a strength defined

--- QgisModelBaker/libqgsprojectgen/dbconnector/pg_connector.py
+++ QgisModelBaker/libqgsprojectgen/dbconnector/pg_connector.py
@@ -422,9 +422,9 @@ class PGConnector(DBConnector):
                             INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KCU2
                              ON KCU2.CONSTRAINT_CATALOG = RC.UNIQUE_CONSTRAINT_CATALOG AND KCU2.CONSTRAINT_SCHEMA = RC.UNIQUE_CONSTRAINT_SCHEMA AND KCU2.CONSTRAINT_NAME = RC.UNIQUE_CONSTRAINT_NAME
                              AND KCU2.ORDINAL_POSITION = KCU1.ORDINAL_POSITION {schema_where2}
-                            INNER JOIN {schema}.t_ili2db_attrname AS ATTRNAME
+                            LEFT JOIN {schema}.t_ili2db_attrname AS ATTRNAME
                              ON ATTRNAME.sqlname = KCU1.COLUMN_NAME AND ATTRNAME.{colowner} = KCU1.TABLE_NAME AND ATTRNAME.target = KCU2.TABLE_NAME
-                            INNER JOIN {schema}.t_ili2db_meta_attrs AS META_ATTRS
+                            LEFT JOIN {schema}.t_ili2db_meta_attrs AS META_ATTRS
                              ON META_ATTRS.ilielement = ATTRNAME.iliname AND META_ATTRS.attr_name = 'ili2db.ili.assocKind'
                             GROUP BY RC.CONSTRAINT_NAME, KCU1.TABLE_NAME, KCU1.COLUMN_NAME, KCU2.CONSTRAINT_SCHEMA, KCU2.TABLE_NAME, KCU2.COLUMN_NAME, KCU1.ORDINAL_POSITION, META_ATTRS.attr_value
                             ORDER BY KCU1.ORDINAL_POSITION
