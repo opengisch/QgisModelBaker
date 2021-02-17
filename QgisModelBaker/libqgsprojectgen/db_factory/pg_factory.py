c8273d54dc6a336fc1bddcb4865648a3dc7dcc37
olivierdalang <olivier.dalang@gmail.com>
2021-02-17 11:29:15 +0100

    push ili2pg to 4.4.6

--- QgisModelBaker/libqgsprojectgen/db_factory/pg_factory.py
+++ QgisModelBaker/libqgsprojectgen/db_factory/pg_factory.py
@@ -86,7 +86,7 @@ class PgFactory(DbFactory):
         if db_ili_version == 3:
             return '3.11.2'
         else:
-            return '4.4.4'
+            return '4.4.6'
 
     def get_tool_url(self, db_ili_version):
         """Returns download url of ili2pg.
