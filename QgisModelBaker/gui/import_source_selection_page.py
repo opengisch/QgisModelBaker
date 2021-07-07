48bec6a45c0dae33a6d9b61d3aa124c62a895920
signedav <david@opengis.ch>
2021-07-07 12:44:06 +0200

    fixes on the gui behavior

--- QgisModelBaker/gui/import_source_selection_page.py
+++ QgisModelBaker/gui/import_source_selection_page.py
@@ -86,7 +86,6 @@ class SourceModel(QStandardItemModel):
         item.setData(name, int(SourceModel.Roles.NAME))
         item.setData(type, int(SourceModel.Roles.TYPE))
         item.setData(path, int(SourceModel.Roles.PATH))
-        print( os.path.join(os.path.dirname(__file__), f'../images/file_types/{type}.png'))
         item.setData(QIcon(os.path.join(os.path.dirname(__file__), f'../images/file_types/{type}.png')), Qt.DecorationRole)
         self.appendRow(item)
     
@@ -107,25 +106,36 @@ class ImportSourceSeletionPage(QWizardPage, PAGE_UI):
 
         self.file_browse_button.clicked.connect( make_file_selector(self.input_line_edit, title=self.tr('Open Interlis Model, Transfer or Catalogue File'), file_filter=self.tr('Interlis Model File (*.ili);;Transfer File (*.xtf *.itf *.XTF *.ITF);;Catalogue File (*.xml *.XML *.xls *.XLS *.xlsx *.XLSX)')))
         
-        self.validators = Validators()
-        fileValidator = FileValidator(pattern=['*.' + ext for ext in self.ValidExtensions], allow_empty=True)
-        self.input_line_edit.setValidator(fileValidator)
+        # self.validators = Validators()
+        # fileValidator = FileValidator(pattern=['*.' + ext for ext in self.ValidExtensions], allow_empty=True)
+        # self.input_line_edit.setValidator(fileValidator)
+        # self.input_line_edit.textChanged.connect(self.validators.validate_line_edits)
 
-        # self.input_line_edit.textChanged.connect( self.validators.validate_line_edits)
         self.ilicache = IliCache(self.base_configuration)
         self.model_delegate = ModelCompleterDelegate()
         self.refresh_ili_models_cache()
         self.input_line_edit.setPlaceholderText(self.tr('[Browse for file or search model from repository]'))
 
-        self.input_line_edit.textChanged.emit( self.input_line_edit.text())
-        self.input_line_edit.textChanged.connect(self.complete_models_completer)
-        self.input_line_edit.punched.connect(self.complete_models_completer)
+        self.input_line_edit.punched.connect(self.first_time_punch)
 
         self.model = SourceModel()
         self.source_list_view.setModel(self.model)
         self.add_button.clicked.connect(self.add_row)
         self.remove_button.clicked.connect(self.remove_selected_rows)
+
+        self.remove_button.setEnabled(self.model.rowCount())
+        self.model.rowsInserted.connect( lambda: self.remove_button.setEnabled(self.model.rowCount()))
+        self.model.rowsRemoved.connect( lambda: self.remove_button.setEnabled(self.model.rowCount()))
+        
+        # self.add_button.setEnabled(False)
         # self.restore_configuration()
+
+    def first_time_punch(self):
+        # might could be nices
+        self.input_line_edit.punched.disconnect(self.first_time_punch)
+        self.input_line_edit.textChanged.emit( self.input_line_edit.text())
+        self.input_line_edit.textChanged.connect(self.complete_models_completer)
+        self.input_line_edit.punched.connect(self.complete_models_completer)
 
     def refresh_ili_models_cache(self):
         # self.ilicache.new_message.connect(self.show_message)
@@ -133,15 +143,16 @@ class ImportSourceSeletionPage(QWizardPage, PAGE_UI):
         self.update_models_completer()
 
     def complete_models_completer(self):
-        if not self.input_line_edit.text():
-            self.input_line_edit.completer().setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-            self.input_line_edit.completer().complete()
-        else:
-            match_contains = self.input_line_edit.completer().completionModel().match(self.input_line_edit.completer().completionModel().index(0, 0),
-                                            Qt.DisplayRole, self.input_line_edit.text(), -1, Qt.MatchContains)
-            if len(match_contains) > 1:
-                self.input_line_edit.completer().setCompletionMode(QCompleter.PopupCompletion)
+        if self.input_line_edit.hasFocus():
+            if not self.input_line_edit.text():
+                self.input_line_edit.completer().setCompletionMode(QCompleter.UnfilteredPopupCompletion)
                 self.input_line_edit.completer().complete()
+            else:
+                match_contains = self.input_line_edit.completer().completionModel().match(self.input_line_edit.completer().completionModel().index(0, 0),
+                                                Qt.DisplayRole, self.input_line_edit.text(), -1, Qt.MatchContains)
+                if len(match_contains) > 1:
+                    self.input_line_edit.completer().setCompletionMode(QCompleter.PopupCompletion)
+                    self.input_line_edit.completer().complete()
 
     def update_models_completer(self):
         completer = QCompleter(self.ilicache.model, self.input_line_edit)
@@ -161,6 +172,8 @@ class ImportSourceSeletionPage(QWizardPage, PAGE_UI):
             type = 'model'
             path = None
         self.source_list_view.model().add_source(name, type, path)
+        self.input_line_edit.clearFocus()
+        self.input_line_edit.clear()
 
     def remove_selected_rows(self):
         indices = self.source_list_view.selectionModel().selectedRows()
