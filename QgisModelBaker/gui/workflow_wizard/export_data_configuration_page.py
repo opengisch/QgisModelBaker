6672a66b9dca1eaa0b644904591984ebe7fa2af8
signedav <david@opengis.ch>
2021-10-28 16:52:54 +0200

    having one table view providing each model (dataset, models, baskets) considering the combobox

--- QgisModelBaker/gui/workflow_wizard/export_data_configuration_page.py
+++ QgisModelBaker/gui/workflow_wizard/export_data_configuration_page.py
@@ -79,59 +79,11 @@ class ExportDataConfigurationPage(QWizardPage, PAGE_UI):
         self.xtf_file_line_edit.textChanged.connect(self._set_current_export_target)
         self.xtf_file_line_edit.textChanged.emit(self.xtf_file_line_edit.text())
 
-        self.filter_combobox.addItem(
-            self.tr("Everything"), ExportDataConfigurationPage.FilterMode.NO_FILTER
-        )
-        self.filter_combobox.addItem(
-            self.tr("Models"), ExportDataConfigurationPage.FilterMode.MODEL
-        )
-        self.filter_combobox.addItem(
-            self.tr("Datasets"), ExportDataConfigurationPage.FilterMode.DATASET
-        )
-        self.filter_combobox.addItem(
-            self.tr("Baskets"), ExportDataConfigurationPage.FilterMode.BASKET
-        )
-
         self.filter_combobox.currentIndexChanged.connect(self._filter_changed)
 
-        self.export_models_checkbox.setCheckState(Qt.Checked)
-        self.export_models_checkbox.stateChanged.connect(self._select_all_models)
-        self.workflow_wizard.export_models_model.dataChanged.connect(
-            lambda: self._set_models_checkbox()
-        )
-        self.export_models_view.setModel(self.workflow_wizard.export_models_model)
-        self.export_models_view.clicked.connect(
-            self.workflow_wizard.export_models_model.check
-        )
-        self.export_models_view.space_pressed.connect(
-            self.workflow_wizard.export_models_model.check
-        )
-
-        self.export_datasets_checkbox.setCheckState(Qt.Checked)
-        self.export_datasets_checkbox.stateChanged.connect(self._select_all_datasets)
-        self.workflow_wizard.export_datasets_model.dataChanged.connect(
-            lambda: self._set_datasets_checkbox()
-        )
-        self.export_datasets_view.setModel(self.workflow_wizard.export_datasets_model)
-        self.export_datasets_view.clicked.connect(
-            self.workflow_wizard.export_datasets_model.check
-        )
-        self.export_datasets_view.space_pressed.connect(
-            self.workflow_wizard.export_datasets_model.check
-        )
-
-        self.export_baskets_checkbox.setCheckState(Qt.Checked)
-        self.export_baskets_checkbox.stateChanged.connect(self._select_all_baskets)
-        self.workflow_wizard.export_baskets_model.dataChanged.connect(
-            lambda: self._set_baskets_checkbox()
-        )
-        self.export_baskets_view.setModel(self.workflow_wizard.export_baskets_model)
-        self.export_baskets_view.clicked.connect(
-            self.workflow_wizard.export_baskets_model.check
-        )
-        self.export_baskets_view.space_pressed.connect(
-            self.workflow_wizard.export_baskets_model.check
-        )
+        self.select_all_checkbox.setCheckState(Qt.Checked)
+        self.select_all_checkbox.stateChanged.connect(self._select_all_items)
+        self._set_export_filter_view_model(self.workflow_wizard.export_models_model)
 
     def isComplete(self):
         return self.is_complete
@@ -145,12 +97,49 @@ class ExportDataConfigurationPage(QWizardPage, PAGE_UI):
         return self.workflow_wizard.next_id()
 
     def setup_dialog(self, basket_handling):
-        self.export_datasets_label.setHidden(not basket_handling)
-        self.export_datasets_checkbox.setHidden(not basket_handling)
-        self.export_datasets_view.setHidden(not basket_handling)
+        self._refresh_filter_combobox(basket_handling)
 
-    def _filter_changed(self):
-        self.filter_combobox.currentData()
+    def _refresh_filter_combobox(self, basket_handling):
+        current_index = self.filter_combobox.currentIndex()
+
+        self.filter_combobox.clear()
+        self.filter_combobox.addItem(
+            self.tr("No filter (export all models)"),
+            ExportDataConfigurationPage.FilterMode.NO_FILTER,
+        )
+        self.filter_combobox.addItem(
+            self.tr("Models"), ExportDataConfigurationPage.FilterMode.MODEL
+        )
+        if basket_handling:
+            self.filter_combobox.addItem(
+                self.tr("Datasets"), ExportDataConfigurationPage.FilterMode.DATASET
+            )
+            self.filter_combobox.addItem(
+                self.tr("Baskets"), ExportDataConfigurationPage.FilterMode.BASKET
+            )
+            self.filter_combobox.setCurrentIndex(current_index)
+
+        self.filter_combobox.setCurrentIndex(
+            current_index if self.filter_combobox.itemData(current_index) else 0
+        )
+        self._filter_changed()
+
+    def _set_export_filter_view_model(self, model):
+        try:
+            self.export_items_view.clicked.disconnect()
+            self.export_items_view.space_pressed.disconnect()
+            self.export_items_view.model().dataChanged.disconnect()
+        except Exception:
+            pass
+
+        self.export_items_view.setModel(model)
+        self.export_items_view.model().dataChanged.connect(
+            lambda: self._set_select_all_checkbox()
+        )
+        self.export_items_view.clicked.connect(self.export_items_view.model().check)
+        self.export_items_view.space_pressed.connect(
+            self.export_items_view.model().check
+        )
 
     def _set_current_export_target(self, text):
         self.setComplete(
@@ -159,31 +148,38 @@ class ExportDataConfigurationPage(QWizardPage, PAGE_UI):
         )
         self.workflow_wizard.current_export_target = text
 
-    def _select_all_models(self, state):
+    def _filter_changed(self):
+        filter = self.filter_combobox.currentData()
+        if filter == ExportDataConfigurationPage.FilterMode.NO_FILTER:
+            self.export_items_view.setHidden(True)
+            self.select_all_checkbox.setHidden(True)
+        else:
+            self.export_items_view.setHidden(False)
+            self.select_all_checkbox.setHidden(False)
+            if filter == ExportDataConfigurationPage.FilterMode.MODEL:
+                self._set_export_filter_view_model(
+                    self.workflow_wizard.export_models_model
+                )
+                self.select_all_checkbox.setText(self.tr("Select all models"))
+            if filter == ExportDataConfigurationPage.FilterMode.DATASET:
+                self._set_export_filter_view_model(
+                    self.workflow_wizard.export_datasets_model
+                )
+                self.select_all_checkbox.setText(self.tr("Select all datasets"))
+            if filter == ExportDataConfigurationPage.FilterMode.BASKET:
+                self._set_export_filter_view_model(
+                    self.workflow_wizard.export_baskets_model
+                )
+                self.select_all_checkbox.setText(self.tr("Select all baskets"))
+            self._set_select_all_checkbox()
+
+    def _select_all_items(self, state):
         if state != Qt.PartiallyChecked:
-            self.workflow_wizard.export_models_model.check_all(state)
+            self.export_items_view.model().check_all(state)
 
-    def _set_models_checkbox(self):
-        self.export_models_checkbox.setCheckState(
-            self._evaluated_check_state(self.workflow_wizard.export_models_model)
-        )
-
-    def _select_all_datasets(self, state):
-        if state != Qt.PartiallyChecked:
-            self.workflow_wizard.export_datasets_model.check_all(state)
-
-    def _set_datasets_checkbox(self):
-        self.export_datasets_checkbox.setCheckState(
-            self._evaluated_check_state(self.workflow_wizard.export_datasets_model)
-        )
-
-    def _select_all_baskets(self, state):
-        if state != Qt.PartiallyChecked:
-            self.workflow_wizard.export_baskets_model.check_all(state)
-
-    def _set_baskets_checkbox(self):
-        self.export_baskets_checkbox.setCheckState(
-            self._evaluated_check_state(self.workflow_wizard.export_baskets_model)
+    def _set_select_all_checkbox(self):
+        self.select_all_checkbox.setCheckState(
+            self._evaluated_check_state(self.export_items_view.model())
         )
 
     def _evaluated_check_state(self, model):
