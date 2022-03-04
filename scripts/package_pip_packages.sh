33fb433874797c71ebd98bac1f5ff81bf9d8a9fa
signedav <david@opengis.ch>
2022-03-04 11:19:13 +0100

    script downloading wheels from pypi and deploying the packages in libs

---
+++ scripts/package_pip_packages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+LIBS_DIR="QgisModelBaker/libs"
+PGSERVICEPARSER=("pgserviceparser" "1.0.1")
+PACKAGING=("packaging" "21.3")
+
+PACKAGES=(
+  PGSERVICEPARSER[@]
+  PACKAGING[@]
+)
+
+#create lib folder
+mkdir -p $LIBS_DIR
+
+for PACKAGE in ${PACKAGES[@]}; do
+  echo download and unpack ${!PACKAGE:0:1} with version ${!PACKAGE:1:1}
+  #create temp folder
+  mkdir -p temp
+  #download the wheel
+  pip download -v ${!PACKAGE:0:1}==${!PACKAGE:1:1} --only-binary :all: -d temp/
+  #unpack the wheel
+  unzip -o temp/${!PACKAGE:0:1}*.whl -d $LIBS_DIR
+  #remove temp folder
+  rm -r temp
+done
