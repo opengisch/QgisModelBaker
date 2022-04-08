5ccb3cfa798f2d5b524f3ac32c10135c0971aea4
signedav <david@opengis.ch>
2022-04-08 09:46:13 +0200

    bump modelbaker version to 1.0.3

--- scripts/package_pip_packages.sh
+++ scripts/package_pip_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LIBS_DIR="QgisModelBaker/libs"
 
-MODELBAKER_LIBRARY=("modelbaker" "1.0.2")
+MODELBAKER_LIBRARY=("modelbaker" "1.0.3")
 PGSERVICEPARSER=("pgserviceparser" "1.0.1")
 PACKAGING=("packaging" "21.3")
 
