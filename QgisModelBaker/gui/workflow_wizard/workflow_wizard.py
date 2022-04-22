589b658093527a3fd45a711c8b079ed14e132fcd
signedav <david@opengis.ch>
2022-04-22 15:37:02 +0200

    fix window size after adding new fields for service configuration

--- QgisModelBaker/gui/workflow_wizard/workflow_wizard.py
+++ QgisModelBaker/gui/workflow_wizard/workflow_wizard.py
@@ -530,7 +530,7 @@ class WorkflowWizardDialog(QDialog):
         self.workflow_wizard = WorkflowWizard(self.iface, self.base_config, self)
         self.workflow_wizard.setStartId(PageIds.Intro)
         self.workflow_wizard.setWindowFlags(Qt.Widget)
-        self.workflow_wizard.setFixedHeight(self.fontMetrics().lineSpacing() * 36)
+        self.workflow_wizard.setFixedHeight(self.fontMetrics().lineSpacing() * 48)
         self.workflow_wizard.setMinimumWidth(self.fontMetrics().lineSpacing() * 48)
         self.workflow_wizard.show()
 
