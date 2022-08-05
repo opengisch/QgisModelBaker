b0079c437b572477616607471bc77c88717355c6
signedav <david@opengis.ch>
2022-08-05 16:18:04 +0200

    layer tree view and model for the settings

--- QgisModelBaker/gui/toppingmaker_wizard/layers_page.py
+++ QgisModelBaker/gui/toppingmaker_wizard/layers_page.py
@@ -18,7 +18,7 @@
  ***************************************************************************/
 """
 
-from qgis.core import QgsProject
+from qgis.core import QgsLayerTree, QgsLayerTreeModel, QgsProject
 from qgis.PyQt.QtWidgets import QWizardPage
 
 import QgisModelBaker.utils.gui_utils as gui_utils
@@ -27,124 +27,101 @@ from QgisModelBaker.utils import gui_utils
 PAGE_UI = gui_utils.get_ui_class("toppingmaker_wizard/layers.ui")
 
 
-from enum import Enum, IntEnum
+from enum import IntEnum
 
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtGui import QIcon, QStandardItem, QStandardItemModel
 
 
 # maybe this model should be in ProjectTopping or at least ToppingMaker
-class LayerModel(QStandardItemModel):
+class LayerModel(QgsLayerTreeModel):
     """
-    Model providing the layers
+    Model providing the layer tree and the settings
     """
-
-    class Roles(Enum):
-        NAME = Qt.UserRole + 1
-        PROVIDER = Qt.UserRole + 2
-        USE_SOURCE = Qt.UserRole + 3
-        USE_STYLE = Qt.UserRole + 5
-        USE_DEFINITION = Qt.UserRole + 6
-
-    def __int__(self):
-        return self.value
 
     class Columns(IntEnum):
         NAME = 0
         USE_STYLE = 1
-        USE_SOURCE = 2
-        USE_DEFINITION = 3
+        USE_DEFINITION = 2
+        USE_SOURCE = 3
 
-    def __init__(self):
-        super().__init__()
-        self.setColumnCount(4)
+    def __init__(self, layertree: QgsLayerTree):
+        super().__init__(layertree)
+        self.use_style_nodes = {}
+        self.use_source_nodes = {}
+        self.use_definition_nodes = {}
+
+    def columnCount(self, parent):
+        return len(LayerModel.Columns)
 
     def flags(self, index):
-        return Qt.ItemIsSelectable | Qt.ItemIsEnabled
+        if index.column() == LayerModel.Columns.NAME:
+            return Qt.ItemIsEnabled
+        if index.column() == LayerModel.Columns.USE_DEFINITION:
+            return Qt.ItemIsUserCheckable | Qt.ItemIsEnabled
+        else:
+            node = self.index2node(index)
+            if not QgsLayerTree.isGroup(node):
+                return Qt.ItemIsUserCheckable | Qt.ItemIsEnabled
+            else:
+                return Qt.NoItemFlags
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            if section == LayerModel.Columns.NAME:
+                return self.tr("Node")
+            if section == LayerModel.Columns.USE_STYLE:
+                return self.tr("Use Style")
+            if section == LayerModel.Columns.USE_DEFINITION:
+                return self.tr("Use Definition")
+            if section == LayerModel.Columns.USE_SOURCE:
+                return self.tr("Use Source")
+        return QgsLayerTreeModel.headerData(self, section, orientation, role)
 
     def data(self, index, role):
-        item = self.item(index.row(), index.column())
-        if item:
-            if role == Qt.DisplayRole:
-                if index.column() == SourceModel.Columns.SOURCE:
-                    return "{}{}".format(
-                        item.data(int(Qt.DisplayRole)),
-                        f" ({item.data(int(SourceModel.Roles.PATH))})"
-                        if item.data(int(SourceModel.Roles.TYPE)) != "model"
-                        else "",
+        if role == Qt.CheckStateRole:
+            if self.index2node(index):
+                if index.column() == LayerModel.Columns.USE_STYLE:
+                    return self.use_style_nodes.get(
+                        self.index2node(index), Qt.Unchecked
                     )
-                if index.column() == SourceModel.Columns.DATASET:
-                    if self.index(index.row(), SourceModel.Columns.IS_CATALOGUE).data(
-                        int(SourceModel.Roles.IS_CATALOGUE)
-                    ):
-                        return "---"
-                    else:
-                        return item.data(int(SourceModel.Roles.DATASET_NAME))
-
-            if role == Qt.DecorationRole:
-                if index.column() == SourceModel.Columns.SOURCE:
-                    type = "data"
-                    if item.data(int(SourceModel.Roles.TYPE)) and item.data(
-                        int(SourceModel.Roles.TYPE)
-                    ).lower() in ["model", "ili", "xtf", "xml"]:
-                        type = item.data(int(SourceModel.Roles.TYPE)).lower()
-                    return QIcon(
-                        os.path.join(
-                            os.path.dirname(__file__),
-                            f"../images/file_types/{type}.png",
-                        )
+                if index.column() == LayerModel.Columns.USE_DEFINITION:
+                    return self.use_definition_nodes.get(
+                        self.index2node(index), Qt.Unchecked
                     )
-            return item.data(int(role))
+                if index.column() == LayerModel.Columns.USE_SOURCE:
+                    return self.use_source_nodes.get(
+                        self.index2node(index), Qt.Unchecked
+                    )
+        if index.column() == LayerModel.Columns.NAME:
+            return QgsLayerTreeModel.data(self, index, role)
 
-    def add_layer(self, name, provider, use_source, use_style, use_definition):
-        item = QStandardItem()
-        item.setData(name, int(Qt.DisplayRole))
-        item.setData(name, int(Roles.Roles.NAME))
-        item.setData(type, int(SourceModel.Roles.TYPE))
-        item.setData(path, int(SourceModel.Roles.PATH))
-        item.setData(origin_info, int(SourceModel.Roles.ORIGIN_INFO))
-        self.appendRow([item, QStandardItem()])
-
-        self.print_info.emit(
-            self.tr("Add source {} ({}) {}").format(
-                name, path if path else "repository", origin_info
-            )
-        )
-        return True
-
-    def setData(self, index, data, role):
-        if index.column() == SourceModel.Columns.IS_CATALOGUE:
-            return QStandardItemModel.setData(
-                self, index, data, int(SourceModel.Roles.IS_CATALOGUE)
-            )
-        if index.column() == SourceModel.Columns.DATASET:
-            return QStandardItemModel.setData(
-                self, index, data, int(SourceModel.Roles.DATASET_NAME)
-            )
-        return QStandardItemModel.setData(self, index, data, role)
-
-    def remove_sources(self, indices):
-        for index in sorted(indices):
-            path = index.data(int(SourceModel.Roles.PATH))
-            self.print_info.emit(
-                self.tr("Remove source {} ({})").format(
-                    index.data(int(SourceModel.Roles.NAME)),
-                    path if path else "repository",
+    def setData(self, index, role, data):
+        if role == Qt.CheckStateRole:
+            if self.index2node(index):
+                if index.column() == LayerModel.Columns.USE_STYLE:
+                    self.use_style_nodes[self.index2node(index)] = data
+                if index.column() == LayerModel.Columns.USE_DEFINITION:
+                    self.use_definition_nodes[self.index2node(index)] = data
+                if index.column() == LayerModel.Columns.USE_SOURCE:
+                    self.use_source_nodes[self.index2node(index)] = data
+                self.dataChanged.emit(
+                    self.index(0, 0),
+                    self.index(self.rowCount(), self.columnCount(self.parent)),
                 )
-            )
-            self.removeRow(index.row())
+                return True
+        return QgsLayerTreeModel.setData(self, index, role, data)
 
-    def _source_in_model(self, name, type, path):
-        match_existing = self.match(
-            self.index(0, 0), int(SourceModel.Roles.NAME), name, -1, Qt.MatchExactly
+    def check(self, index):
+        if index.flags() & (Qt.ItemIsUserCheckable | Qt.ItemIsEnabled):
+            if self.data(index, Qt.CheckStateRole) == Qt.Checked:
+                self.setData(index, Qt.CheckStateRole, Qt.Unchecked)
+            else:
+                self.setData(index, Qt.CheckStateRole, Qt.Checked)
+
+    def _emit_data_changed(self):
+        self.dataChanged.emit(
+            self.index(0, 0), self.index(self.rowCount(), self.columnCount(self.parent))
         )
-        if (
-            match_existing
-            and type == match_existing[0].data(int(SourceModel.Roles.TYPE))
-            and path == match_existing[0].data(int(SourceModel.Roles.PATH))
-        ):
-            return True
-        return False
 
 
 class LayersPage(QWizardPage, PAGE_UI):
@@ -157,14 +134,14 @@ class LayersPage(QWizardPage, PAGE_UI):
 
         self.setTitle(title)
 
-        self.layer_table_view.setModel(
-            self.topping_wizard.topping_maker.project_topping.model
-        )
+        self.layermodel = LayerModel(QgsProject.instance().layerTreeRoot())
+        self.layermodel.setFlags(QgsLayerTreeModel.Flags())
+        self.layer_table_view.setModel(self.layermodel)
+        self.layer_table_view.resizeColumnToContents(LayerModel.Columns.NAME)
+        self.layer_table_view.expandAll()
+        self.layer_table_view.clicked.connect(self.layer_table_view.model().check)
 
     def initializePage(self) -> None:
-        self.toppingmaker_wizard.topping_maker.create_projecttopping(
-            QgsProject.instance()
-        )
         return super().initializePage()
 
     def validatePage(self) -> bool:
