efe7e20db54b722297caffca2a8fb8bc2aa4a5f5
signedav <david@opengis.ch>
2023-12-21 12:24:18 +0100

    keep layer tree export settings

--- QgisModelBaker/gui/topping_wizard/layers_page.py
+++ QgisModelBaker/gui/topping_wizard/layers_page.py
@@ -106,7 +106,7 @@ class LayerModel(QgsLayerTreeModel):
         self.use_definition_nodes = {}
         self.ili_schema_identificators = []
 
-        self.reload()
+        self.reload(True)
 
     def columnCount(self, parent=None):
         return len(LayerModel.Columns)
@@ -311,9 +311,10 @@ class LayerModel(QgsLayerTreeModel):
             else:
                 self.setData(index, Qt.CheckStateRole, Qt.Checked)
 
-    def reload(self):
+    def reload(self, load_defaults=False):
         self._load_ili_schema_identificators()
-        self._set_default_values()
+        if load_defaults:
+            self._set_default_values()
 
     def _disable_children(self, parent: QModelIndex):
         for child_row in range(self.rowCount(parent)):
@@ -553,7 +554,7 @@ class LayersPage(QWizardPage, PAGE_UI):
 
         self.categories_dialog = LayerStyleCategoriesDialog()
         self.stylecat_delegate.button_clicked.connect(self.open_categories_dialog)
-        self.reset_button.clicked.connect(self.layermodel.reload)
+        self.reset_button.clicked.connect(lambda: self.layermodel.reload(True))
 
     def open_categories_dialog(self, index):
         layername = index.data(int(Qt.DisplayRole))
