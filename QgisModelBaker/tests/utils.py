91735243ead02d67cc8daa4bd75840cdc31c335c
Germán Carrillo <gcarrillo@linuxmail.org>
2024-06-06 13:38:11 -0500

    [tests] Test that PG service settings are remembered, as well as additional settings given by users like schema, ssl-mode and auth config

--- QgisModelBaker/tests/utils.py
+++ QgisModelBaker/tests/utils.py
@@ -4,6 +4,6 @@ import pytest
 
 
 @pytest.mark.skip("This is a utility function, not a test function")
-def testdata_path(path):
+def _testdata_path(path):
     basepath = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(basepath, "testdata", path)
