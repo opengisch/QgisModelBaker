73c491da0910cacf27369d777a2d024ed414acc3
Germán Carrillo <gcarrillo@linuxmail.org>
2024-08-26 18:40:07 -0500

    [gui] Introduce brand new new Basket Manager (still partial)

---
+++ QgisModelBaker/gui/panel/summary_basket_panel.py
@@ -0,0 +1,217 @@
+"""
+/***************************************************************************
+                              -------------------
+        begin                : 26.08.2024
+        git sha              : :%H$
+        copyright            : (C) 2024 by Germán Carrillo
+        email                : german at opengis ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import uuid
+from enum import Enum, IntEnum
+
+from qgis.PyQt.QtCore import QAbstractTableModel, QModelIndex, Qt
+from qgis.PyQt.QtWidgets import QHeaderView, QWidget
+
+import QgisModelBaker.utils.gui_utils as gui_utils
+
+WIDGET_UI = gui_utils.get_ui_class("basket_panel.ui")
+
+
+class BasketModel(QAbstractTableModel):
+    """
+    ItemModel providing all the baskets, the existing ones and the possible ones in the given dataset.
+    It provides the topic, the suggested BIDs (t_ili_tid) and the information if it's created already.
+    As well the option if it should be created (could be extended if it should be deleted when unchecked)
+    """
+
+    class Roles(Enum):
+        EXISTING = Qt.UserRole + 1
+
+        def __int__(self):
+            return self.value
+
+    class Columns(IntEnum):
+        DATASET = 0
+        TOPIC = 1
+        BID_DOMAIN = 2
+        BID_VALUE = 3
+        ATTACHMENT_KEY = 4
+
+    def __init__(self):
+        super().__init__()
+        self.basket_settings = {}
+
+    def columnCount(self, parent):
+        return len(BasketModel.Columns)
+
+    def rowCount(self, parent):
+        return len(self.basket_settings.keys())
+
+    def flags(self, index):
+        return Qt.ItemIsSelectable | Qt.ItemIsEnabled
+
+    def index(self, row: int, column: int, parent: QModelIndex = ...) -> QModelIndex:
+        """
+        default override
+        """
+        return super().createIndex(row, column, parent)
+
+    def parent(self, index):
+        """
+        default override
+        """
+        return index
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            if section == BasketModel.Columns.DATASET:
+                return self.tr("Dataset")
+            if section == BasketModel.Columns.TOPIC:
+                return self.tr("Topic")
+            if section == BasketModel.Columns.BID_DOMAIN:
+                return self.tr("BID (OID Type)")
+            if section == BasketModel.Columns.BID_VALUE:
+                return self.tr("BID Value")
+            if section == BasketModel.Columns.ATTACHMENT_KEY:
+                return self.tr("Attachment key")
+
+    def data(self, index, role):
+        if role == int(Qt.DisplayRole) or role == int(Qt.EditRole):
+            key = list(self.basket_settings.keys())[index.row()]
+            if index.column() == BasketModel.Columns.DATASET:
+                return self.basket_settings[key]["dataset"]
+            if index.column() == BasketModel.Columns.TOPIC:
+                return key
+            if index.column() == BasketModel.Columns.BID_DOMAIN:
+                return self.basket_settings[key]["bid_domain"] or "---"
+            if index.column() == BasketModel.Columns.BID_VALUE:
+                return self.basket_settings[key]["bid_value"]
+            if index.column() == BasketModel.Columns.ATTACHMENT_KEY:
+                return self.basket_settings[key]["attachment_key"]
+        elif role == int(Qt.ToolTipRole):
+            key = list(self.basket_settings.keys())[index.row()]
+            if index.column() == BasketModel.Columns.DATASET:
+                return self.tr("Dataset this basket belongs to")
+            if index.column() == BasketModel.Columns.TOPIC:
+                return key
+            if index.column() == BasketModel.Columns.BID_DOMAIN:
+                message = self.tr(
+                    "<html><head/><body><p>The OID format is not defined, you can use whatever you want, but it should always start with an underscore <code>_</code> or an alphanumeric value.</p></body></html>"
+                )
+                oid_domain = self.basket_settings[key].get("oid_domain", "")
+                if oid_domain[-7:] == "UUIDOID":
+                    message = self.tr(
+                        "<html><head/><body><p>The OID should be an Universally Unique Identifier (OID TEXT*36).</p></body></html>"
+                    )
+                elif oid_domain[-11:] == "STANDARDOID":
+                    message = self.tr(
+                        """<html>
+                        <body>
+                        <p>
+                        The OID format requireds an 8 char prefix and 8 char postfix.
+                        </p>
+                        <p><b>Prefix (2 + 6 chars):</b> Country identifier + a 'global' identification part assigned once by the official authority.</p>
+                        </p><p><b>Postfix (8 chars):</b> Sequence (numeric or alphanumeric) of your system as 'local' identification part.</p>
+                        </body>
+                        </html>
+                """
+                    )
+                elif oid_domain[-6:] == "I32OID":
+                    message = self.tr(
+                        "<html><head/><body><p>The OID must be an integer value (OID 0 .. 2147483647).</p></body></html>"
+                    )
+                elif oid_domain[-6:] == "ANYOID":
+                    message = self.tr(
+                        "<html><head/><body><p>The OID format could vary depending in what basket the object (entry) is located.</p><p>These objects could be in the following topics: {topics}</body></html>".format(
+                            topics=self.basket_settings[key]["interlis_topic"]
+                        )
+                    )
+                return message
+            if index.column() == BasketModel.Columns.BID_VALUE:
+                return "<html><head/><body><p>Use `{t_id}` as placeholder when you want to use the next T_Id sequence value.</body></html>"
+            if index.column() == BasketModel.Columns.ATTACHMENT_KEY:
+                return "Attachment key"
+        elif role == int(BasketModel.Roles.EXISTING):
+            key = list(self.basket_settings.keys())[index.row()]
+            return self.basket_settings[key]["existing"]
+        return None
+
+    def load_basket_config(self, db_connector, dataset):
+        self.beginResetModel()
+        self.basket_settings.clear()
+        for topic_record in db_connector.get_topics_info():
+            basket_setting = {}
+
+            topic_key = f"{topic_record['model']}.{topic_record['topic']}"
+            basket_setting["attachment_key"] = ""
+            # check if existing
+            existing = False
+            for basket_record in db_connector.get_baskets_info():
+                if (
+                    basket_record["datasetname"] == dataset
+                    and topic_key == basket_record["topic"]
+                ):
+                    existing = True
+                    basket_setting["bid_value"] = basket_record["basket_t_ili_tid"]
+                    basket_setting["attachment_key"] = basket_record["attachmentkey"]
+
+            basket_setting["dataset"] = dataset
+
+            # if not existing "suggest" create if "relevant"
+            basket_setting["existing"] = existing
+            basket_setting["create"] = existing or topic_record["relevance"]
+
+            basket_setting["bid_domain"] = topic_record["bid_domain"]
+
+            if not existing:
+                # set suggestion of value
+                if basket_setting["bid_domain"] == "INTERLIS.UUIDOID":
+                    basket_setting["bid_value"] = str(uuid.uuid4())
+                elif basket_setting["bid_domain"] == "INTERLIS.STANDARDOID":
+                    basket_setting["bid_value"] = "%change%{t_id}"
+                elif basket_setting["bid_domain"] == "INTERLIS.I32OID":
+                    basket_setting["bid_value"] = "{t_id}"
+                else:
+                    basket_setting["bid_value"] = f"_{uuid.uuid4()}"
+
+            self.basket_settings[topic_key] = basket_setting
+        self.endResetModel()
+
+
+class SummaryBasketPanel(QWidget, WIDGET_UI):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self.setupUi(self)
+        self.parent = parent
+        self.bid_model = BasketModel()
+        self.basket_view.setModel(self.bid_model)
+
+        self.basket_view.horizontalHeader().setSectionResizeMode(
+            BasketModel.Columns.DATASET, QHeaderView.ResizeToContents
+        )
+        self.basket_view.horizontalHeader().setSectionResizeMode(
+            BasketModel.Columns.TOPIC, QHeaderView.Stretch
+        )
+        self.basket_view.horizontalHeader().setSectionResizeMode(
+            BasketModel.Columns.BID_DOMAIN, QHeaderView.ResizeToContents
+        )
+        self.basket_view.horizontalHeader().setSectionResizeMode(
+            BasketModel.Columns.BID_VALUE, QHeaderView.ResizeToContents
+        )
+        self.basket_view.horizontalHeader().setSectionResizeMode(
+            BasketModel.Columns.ATTACHMENT_KEY, QHeaderView.ResizeToContents
+        )
+
+    def load_basket_config(self, db_connector, dataset):
+        self.bid_model.load_basket_config(db_connector, dataset)
