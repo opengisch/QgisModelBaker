b283070236795fc9f9577c1a863236e0a0ee27bc
signedav <david@opengis.ch>
2025-08-29 09:22:49 +0200

    unittests

--- QgisModelBaker/tests/test_guiutils.py
+++ QgisModelBaker/tests/test_guiutils.py
@@ -62,3 +62,50 @@ class TestGuiUtils(unittest.TestCase):
             "RoadsSimple",
             "Waldreservate_V2_0",
         }
+
+    def test_model_summarize(self):
+        """Test if the detected models are properly summarized, so the same model don't appear multiple times in the importmodels model."""
+
+        transferfiles = [
+            testdata_path("xtf/23_ciaf_ladm.xtf"),
+            testdata_path("xtf/23_roads_simple.xtf"),
+            testdata_path("xtf/24_waldreservate_V2_0.xtf"),
+            testdata_path("xtf/24_waldreservate_catalogues_V2_0.xml"),
+        ]
+
+        ilifiles = [
+            testdata_path("ilimodels/RoadsSimple.ili"),
+            testdata_path("ilimodels/Beispiel1.ili"),
+            testdata_path("ilimodels/Waldreservate_V2_0.ili"),
+        ]
+
+        source_model = SourceModel()
+
+        for file in transferfiles:
+            source_model.add_source(
+                pathlib.Path(file).name,
+                pathlib.Path(file).suffix[1:],
+                file,
+                "unit tests",
+            )
+
+        for file in ilifiles:
+            source_model.add_source(
+                pathlib.Path(file).name,
+                pathlib.Path(file).suffix[1:],
+                file,
+                "unit tests",
+            )
+
+        import_models_model = ImportModelsModel()
+
+        import_models_model.refresh_model(source_model)
+
+        assert set(import_models_model.checked_models()) == {
+            "Beispiel1",  # from ili only
+            "CIAF_LADM",  # from xtf only
+            "Catastro_COL_ES_V2_1_6",  # from xtf only
+            "ISO19107_V1_MAGNABOG",  # from xtf only
+            "RoadsSimple",  # from ili and from xtf
+            "Waldreservate_V2_0",  # from ili and from xtf
+        }
