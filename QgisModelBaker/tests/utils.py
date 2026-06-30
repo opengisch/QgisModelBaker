7e226fbfb1b8d2ffb5961004aa2695a3faf17f88
signedav <david@opengis.ch>
2026-06-30 16:52:28 +0200

    remove tests

--- QgisModelBaker/tests/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-
-
-@pytest.mark.skip("This is a utility function, not a test function")
-def testdata_path(path):
-    basepath = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(basepath, "testdata", path)
