7093cf170e560e99dbdc4f1e7942c8f429958d41
signedav <david@opengis.ch>
2026-09-23 08:08:59 +0200

    Apply Basket Creation and Data Connection Algorithms.

--- QgisModelBaker/processing_provider/provider.py
+++ QgisModelBaker/processing_provider/provider.py
@@ -3,6 +3,10 @@ import os
 from qgis.core import QgsProcessingProvider
 from qgis.PyQt.QtGui import QIcon
 
+from QgisModelBaker.libs.modelbaker.processing.db_basket_creating import (
+    BasketCreatingGPKGAlgorithm,
+    BasketCreatingPGAlgorithm,
+)
 from QgisModelBaker.libs.modelbaker.processing.ili2db_exporting import (
     ExportingGPKGAlgorithm,
     ExportingPGAlgorithm,
@@ -19,6 +23,10 @@ from QgisModelBaker.libs.modelbaker.processing.ili2db_validating import (
     ValidatingGPKGAlgorithm,
     ValidatingPGAlgorithm,
 )
+from QgisModelBaker.libs.modelbaker.processing.util_dataconnectionparsing import (
+    DataConnectionParsingGPKGAlgorithm,
+    DataConnectionParsingPGAlgorithm,
+)
 from QgisModelBaker.libs.modelbaker.processing.util_layersourceparsing import (
     LayerSourceParsingAlgorithm,
 )
@@ -34,7 +42,11 @@ class Provider(QgsProcessingProvider):
         self.addAlgorithm(ExportingGPKGAlgorithm())
         self.addAlgorithm(ValidatingPGAlgorithm())
         self.addAlgorithm(ValidatingGPKGAlgorithm())
+        self.addAlgorithm(BasketCreatingPGAlgorithm())
+        self.addAlgorithm(BasketCreatingGPKGAlgorithm())
         self.addAlgorithm(LayerSourceParsingAlgorithm())
+        self.addAlgorithm(DataConnectionParsingPGAlgorithm())
+        self.addAlgorithm(DataConnectionParsingGPKGAlgorithm())
 
     def id(self) -> str:
         return "modelbaker"
